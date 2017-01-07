058652f317b054ec074b26a1f4403076fa23bc44
amalegaonkar <amalegaonkar@gmail.com>
2017-01-07 15:06:20 -0800

    Add files via upload

---
+++ createEnv.sh
@@ -0,0 +1,5 @@
+#install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+brew install python
+sudo pip install -r requirements.txt
